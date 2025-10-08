9ea905313c16dafe5bec91f352bedde8b7f98b1b
0xKeynara <75882474+yusufwardana@users.noreply.github.com>
2025-10-08 09:13:54 +0700

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import TikTokAffiliateAutoContentGenerator from './components/TikTokAffiliateAutoContentGenerator';
+
+function App() {
+  return (
+    <div>
+      <TikTokAffiliateAutoContentGenerator />
+    </div>
+  )
+}
+
+export default App;
+
